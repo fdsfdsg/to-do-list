b976618a3ae580328e5eeef34feb7e331538efd8
park sangbeom <smctkdqja@gmail.com>
2020-01-19 23:56:00 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,66 @@
 import React, { Component } from 'react';
 import TodoListTemplate from './components/TodoListTemplate';
 import Form from './components/Form'
+import TodoItemList from './components/TodoItemList'
 
 class App extends Component {
+
+  id = 3
+
+  state = {
+    input: ' ',
+    todos : [
+      { id: 0, text: 'to-do list 공부하기', checked: false },
+      { id: 1, text: '노마더코드 react 마무리하기', checked: true },
+      { id: 2, text: '자기소개서 첨삭', checked: false }
+    ]
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      input: e.target.value // input 의 다음 바뀔 값
+    });
+  }
+
+  handleCreate = () => {
+    const { input, todos } = this.state;
+    this.setState({
+      input: '', // 인풋 비우고
+      // concat 을 사용하여 배열에 추가
+      todos: todos.concat({
+        id: this.id++,
+        text: input,
+        checked: false
+      })
+    });
+  }
+
+  handleKeyPress = (e) => {
+    // 눌려진 키가 Enter 면 handleCreate 호출
+    if (e.key === 'Enter') {
+      this.handleCreate();
+    }
+  }
+
   render() {
+    const { input , todos} = this.state;
+    const {
+      handleChange,
+      handleCreate,
+      handleKeyPress
+    } = this;
+
     return (
-      <TodoListTemplate form={<Form />}>
-        템플릿 완성
-    </TodoListTemplate>
+      <TodoListTemplate form={(
+        <Form
+          value={input}
+          onKeyPress={handleKeyPress}
+          onChange={handleChange}
+          onCreate={handleCreate}
+        />        
+      )}>
+        <TodoItemList todos={todos} />
+      </TodoListTemplate>
     );
   }
 }
--- src/components/TodoItemList.js
+++ src/components/TodoItemList.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+import TodoItem from './TodoItem';
 
 class TodoItemList extends Component {
-    render(){
+    render() {
         const { todos, onToggle, onRemove } = this.props;
 
-        return(
-            <div>
+        const todoList = todos.map(
+            ({ id, text, checked }) => (
+                <TodoItem
+                    id={id}
+                    text={text}
+                    checked={checked}
+                    onToggle={onToggle}
+                    onRemove={onRemove}
+                    key={id}
+                />
+            )
+        );
 
+        return (
+            <div>
+               { todoList } 
             </div>
+            
         );
     }
 }
