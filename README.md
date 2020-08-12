bc8020eb0c400db809b9aff4cc82955cf001be16
park sangbeom <smctkdqja@gmail.com>
2020-08-13 01:42:37 +0900

    refactor: renewal version

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## create-reactp-app 으로 만든 to-do-list
+## 📘 to-do-list
 [![Netlify Status](https://api.netlify.com/api/v1/badges/d7df8cdb-8e46-42d5-bc22-25187dfb68fe/deploy-status)](https://app.netlify.com/sites/too-do-list/deploys)
 ***
-- [FLEX Layout practice game](http://flexboxfroggy.com/#ko)
+- useCallback, useMemo, React.memo 로 컴포넌트 성능 개선하기
--- src/App.js
+++ src/App.js
@@ -1,123 +1,63 @@
-import React, { Component } from 'react';
-import TodoListTemplate from './components/TodoListTemplate';
-import Form from './components/Form'
-import TodoItemList from './components/TodoItemList'
+import React, { useState, useRef, useCallback } from "react";
+import TodoTemplate from "./components/TodoTemplate";
+import TodoInsert from "./components/TodoInsert";
+import TodoList from "./components/TodoList";
 
-class App extends Component {
+const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "리액트 공부하기",
+      checked: true,
+    },
+    {
+      id: 2,
+      text: "useCallback, useMemo, React.memo 로 컴포넌트 성능 개선하기",
+      checked: true,
+    },
+    {
+      id: 3,
+      text: "리액트 연락처 웹 리뉴얼하기",
+      checked: false,
+    },
+  ]);
 
-  id = 2
+  const nextId = useRef(4);
 
-  state = {
-    input: ' ',
-    todos : [
-      {
-        text: "react 공부하기",
-        id: 0
-      },
-      {
-        text: "express 공부하기",
-        id: 1
-      }
-    ]
-  }
-  
-  UNSAFE_componentWillMount() {
-    const todos = localStorage.todos;
-    const id = localStorage.id;
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text: text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos]
+  );
 
-    if (todos) {
-      this.setState({
-        todos: JSON.parse(todos),
-        id,
-      });
-    }
-  }
+  const onRemove = useCallback(
+    (id) => {
+      setTodos(todos.filter((todo) => todo.id !== id));
+    },
+    [todos]
+  );
 
-  componentDidUpdate(prevProps, prevState) {
-    if (JSON.stringify(prevState.todos) !== JSON.stringify(this.state.todos)) {
-      localStorage.todos = JSON.stringify(this.state.todos)
-    }
-    if (prevState.id !== this.state.id) {
-      localStorage.id = this.state.id
-    }
-  }
-
-  handleChange = (e) => {
-    this.setState({
-      input: e.target.value // input 의 다음 바뀔 값
-    });
-  }
-
-  handleCreate = () => {
-    const { input, todos} = this.state;
-    this.setState({
-      input: '', // 인풋 비우고
-      // concat 을 사용하여 배열에 추가
-      todos: todos.concat({
-        id: this.id++,
-        text: input,
-        checked: false
-      })
-    });
-  }
-
-  handleKeyPress = (e) => {
-    // 눌려진 키가 Enter 면 handleCreate 호출
-    if (e.key === 'Enter') {
-      this.handleCreate();
-    }
-  }
-  
-  handleToggle = (id) => {
-    const { todos} = this.state;
-
-    // 파라미터로 받은 id 를 가지고 몇번째 아이템인지 찾습니다.
-    const index = todos.findIndex(todo => todo.id === id);
-    const selected = todos[index]; // 선택한 객체
-
-    const nextTodos = [...todos]; // 배열을 복사
-
-    // 기존의 값들을 복사하고, checked 값을 덮어쓰기
-    nextTodos[index] = {
-      ...selected,
-      checked: !selected.checked
-    };
-
-    this.setState({
-      todos: nextTodos
-    });
-  }
-
-  handleRemove = (id) => {
-    const { todos } =this.state;
-    this.setState({
-      todos: todos.filter(todo => todo.id !== id)
-    })
-  }
-
-  render() {
-    const { input , todos } = this.state;
-    const {
-      handleChange,
-      handleCreate,
-      handleKeyPress,
-      handleToggle,
-      handleRemove
-    } = this;
-
-    return (
-      <TodoListTemplate form={(
-        <Form 
-          value={input}
-          onKeyPress={handleKeyPress}
-          onChange={handleChange}
-          onCreate={handleCreate}
-        />        
-      )}>
-        <TodoItemList todos={todos} onToggle={handleToggle} onRemove={handleRemove}/>
-      </TodoListTemplate>
+  const onToggle = useCallback((id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, checked: !todo.checked } : todo
+      )
     );
-  }
-}
+  },[todos]);
+
+  return (
+    <TodoTemplate>
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
+    </TodoTemplate>
+  );
+};
 
 export default App;
